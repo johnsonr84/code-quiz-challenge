98c814d7abb9975599699022ae24919b4afe2509
Your Name <you@example.com>
2021-07-23 22:34:27 -0600

    add questions.js to project

---
+++ questions.js
@@ -0,0 +1,111 @@
+const questions = [
+  {
+    number: 1,
+    question: 'Which operator is used to assign a value to a variable?',
+    answer: "=",
+    options: [
+      "+", 
+      "=", 
+      "*", 
+      "x"
+    ]
+  },
+  {
+    number: 2,
+    question: 'How do you create a function in JavaScript?',
+    answer: "function myFunction()",
+    options: [
+      "function = myFunction()", 
+      "function myFunction()", 
+      "function:myFunction()", 
+      "function !== myFunction()"
+    ]
+  },
+  {
+    number: 3,
+    question: 'Inside which HTML element do we put the JavaScript?',
+    answer: "<script>",
+    options: [
+      "<scripting>", 
+      "<js>", 
+      "<script>", 
+      "<javascript>"
+    ]
+  },
+  {
+    number: 4,
+    question: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
+    answer: "if (i != 5)",
+    options: [
+      "if (i <> 5)", 
+      "if i <> 5", 
+      "if i =! 5 then", 
+      "if (i != 5)"
+    ]
+  },
+  {
+    number: 5,
+    question: 'Commonly used data types DO NOT Include?',
+    answer: "alerts",
+    options: [
+      "Strings", 
+      "booleans", 
+      "alerts", 
+      "numbers"
+    ]
+  },
+  {
+    number: 6,
+    question: 'How does a FOR loop start?',
+    answer: "for (i = 0; i <= 5; i++)",
+    options: [
+      "for (i <= 5; i++)",
+      "for i = 1 to 5",
+      "for (i = 0; i <= 5)",
+      "for (i = 0; i <= 5; i++)"
+    ]
+  },
+  {
+    number: 7,
+    question: 'How to write an IF statement in JavaScript?',
+    answer: "if (i == 5)",
+    options: [
+      "if (i == 5)",
+      "if i == 5 then",
+      "if i = 5 then",
+      "if i = 5"
+    ]
+  },
+  {
+    number: 8,
+    question: 'Where is the correct place to insert a JavaScript?',
+    answer: "Both the <head> section and the <body> section are correct",
+    options: [
+      "The <body> section",
+      "The <head> section",
+      "Both the <head> section and the <body> section are correct"
+    ]
+  },
+  {
+    number: 9,
+    question: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
+    answer: "if (i != 5)",
+    options: [
+      "if (i != 5)",
+      "if i <> 5",
+      "if i =! 5 then",
+      "if(i <> 5)"
+    ]
+  },
+  {
+    number: 10,
+    question: 'How do you write "Hello World" in an alert box?',
+    answer: "alert('Hello World');",
+    options: [
+      "msgBox('Hello World');",
+      "msg('Hello World');",
+      "alert('Hello World');",
+      "alertBox('Hello World');"
+    ]
+  },
+]
