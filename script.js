2ecb1c39c2241ff36610a03494cdb26e62b006b2
Your Name <you@example.com>
2021-07-26 22:53:08 -0600

    set continue button to activate quiz

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const start_btn = document.querySelector(".start_btn button")
 const info_box = document.querySelector('.info_box')
 const exit_btn = info_box.querySelector('.buttons .quit')
 const continue_btn = info_box.querySelector('.buttons .restart')
+const quiz_box = document.querySelector('.quiz_box')
 
 // Start Button
 start_btn.onclick = () => {
@@ -11,4 +12,10 @@ start_btn.onclick = () => {
 // Exit Button
 exit_btn.onclick = () => {
   info_box.classList.remove("activeInfo"); // hides the info box
+}
+
+// Continue Button
+continue_btn.onclick = () => {
+  info_box.classList.remove("activeInfo"); // hides the info box
+  quiz_box.classList.add("activeQuiz"); // show quiz box
 }
