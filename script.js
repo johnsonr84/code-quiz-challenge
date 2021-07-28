03056bf84907d460d2bb71d09c98fff4d4252818
Your Name <robertcjohnson1984@gmail.com>
2021-07-28 13:32:32 -0600

    get restart quiz button working

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const start_btn = document.querySelector(".start_btn button")
-const info_box = document.querySelector('.info_box')
-const exit_btn = info_box.querySelector('.buttons .quit')
-const continue_btn = info_box.querySelector('.buttons .restart')
-const quiz_box = document.querySelector('.quiz_box')
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
 const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
 
 // Start Button
 start_btn.onclick = () => {
@@ -16,15 +20,52 @@ exit_btn.onclick = () => {
 }
 
 // Continue Button
-continue_btn.onclick = () => {
-  info_box.classList.remove("activeInfo"); // hides the info box
-  quiz_box.classList.add("activeQuiz"); // show quiz box
-  showQuestions(0);
-  queCounter(1);
+continue_btn.onclick = ()=>{
+  info_box.classList.remove("activeInfo"); //hide info box
+  quiz_box.classList.add("activeQuiz"); //show quiz box
+  showQuetions(0); //calling showQestions function
+  queCounter(1); //passing 1 parameter to queCounter
+  startTimer(15); //calling startTimer function
+  startTimerLine(0); //calling startTimerLine function
 }
 
+let timeValue =  15;
 let que_count = 0;
 let que_numb = 1;
+let userScore = 0;
+let counter;
+let counterLine;
+let widthValue = 0;
+
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+// if restartQuiz button clicked
+restart_quiz.onclick = ()=>{
+  quiz_box.classList.add("activeQuiz"); //show quiz box
+  result_box.classList.remove("activeResult"); //hide result box
+  timeValue = 15; 
+  que_count = 0;
+  que_numb = 1;
+  userScore = 0;
+  widthValue = 0;
+  showQuetions(que_count); //calling showQestions function
+  queCounter(que_numb); //passing que_numb value to queCounter
+  clearInterval(counter); //clear counter
+  clearInterval(counterLine); //clear counterLine
+  startTimer(timeValue); //calling startTimer function
+  startTimerLine(widthValue); //calling startTimerLine function
+  timeText.textContent = "Time Left"; //change the text of timeText to Time Left
+  next_btn.classList.remove("show"); //hide the next button
+}
+
+// if quitQuiz button clicked
+quit_quiz.onclick = ()=>{
+  window.location.reload(); //reload the current window
+}
+const next_btn = document.querySelector("footer .next_btn");
+const bottom_ques_counter = document.querySelector("footer .total_que");
+
 
 const next_btn = quiz_box.querySelector(".next_btn");
 
@@ -57,6 +98,9 @@ function showQuestions(index) {
   }
 }
 
+let tickIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
 function optionSelected(answer) {
   let userAns = answer.textContent;
   let correctAns = questions[que_count].answer;
@@ -64,12 +108,18 @@ function optionSelected(answer) {
   if(userAns == correctAns) {
     answer.classList.add("correct");
     console.log("Correct");
+    answer.insertAdjacentHTML("beforeend", tickIcon);
   }else {
     answer.classList.add("incorrect");
     console.log("Incorrect");
+    answer.insertAdjacentHTML("beforeend", crossIcon);
   }
+  // shows correct answer if incorrect answer is selected
   for (let i = 0; i < allOptions; i++) {
-    option_list.children[i].classList.add("disabled")
+    if(option_list.children[i].textCOntent == correctAns) {
+      option_list.children[i].setAttribute("class", "option correct")
+      option_list.children[i].insertAdjacentHTML("beforeend", tickIcon);
+    }
   }
 }
 
