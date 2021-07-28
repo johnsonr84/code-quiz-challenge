5d2c945b651f21a3ac6b56f91467405b83edba38
Your Name <robertcjohnson1984@gmail.com>
2021-07-28 10:43:42 -0600

    add disabel feature to only allow one selection

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const info_box = document.querySelector('.info_box')
 const exit_btn = info_box.querySelector('.buttons .quit')
 const continue_btn = info_box.querySelector('.buttons .restart')
 const quiz_box = document.querySelector('.quiz_box')
+const option_list = document.querySelector(".option_list");
 
 // Start Button
 start_btn.onclick = () => {
@@ -42,7 +43,7 @@ next_btn.onclick = () => {
 // Questions from options array 
 function showQuestions(index) {
   const que_text = document.querySelector(".que_text");
-  const option_list = document.querySelector(".option_list");
+  
   let que_tag = '<span>'+ questions[index].numb + ") " + questions[index].question +'</span>';
   let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
                    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
@@ -50,6 +51,26 @@ function showQuestions(index) {
                    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
   que_text.innerHTML = que_tag;
   option_list.innerHTML = option_tag;
+  const option = option_list.querySelectorAll(".option")
+  for (let i = 0; i < option.length; i++) {
+    option[i].setAttribute("onclick", "optionSelected(this)");
+  }
+}
+
+function optionSelected(answer) {
+  let userAns = answer.textContent;
+  let correctAns = questions[que_count].answer;
+  let allOptions = option_list.children.length;
+  if(userAns == correctAns) {
+    answer.classList.add("correct");
+    console.log("Correct");
+  }else {
+    answer.classList.add("incorrect");
+    console.log("Incorrect");
+  }
+  for (let i = 0; i < allOptions; i++) {
+    option_list.children[i].classList.add("disabled")
+  }
 }
 
 function queCounter(index) {
